ae0c4df8376d31f8d66278cccc306b3c8d8d2595
Cristopher <cadasmeq@gmail.com>
2020-12-23 10:23:16 -0300

    clase 2 : utilizando JSX

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const element = <h1>Hello, World</h1>
+
+// const name = "cristopher"
+// const sum = () => 3 +3 ;
+// const jsx = <h1>Hola soy, {sum()}</h1>
+
+const jsx = 
+    <div>
+        <h1>Hola, soy Cristopher</h1>
+        <p>Soy ingeniero (mentira) frontend.</p>
+    </div>;
+
+const element = React.createElement(
+    'div', 
+    {}, 
+    React.createElement("h1", {}, "Hola, soy Cristopher!"),
+    React.createElement("p", {}, "soy ingeniero (mentira) frontend")
+)
 const container = document.getElementById('app')
 
-ReactDOM.render(element, container)
+ReactDOM.render(jsx, container)
