29ce65b255277214756f72b41fa4f72287f52164
Cristopher <cadasmeq@gmail.com>
2020-12-28 20:22:31 -0300

    clase 19 : Hooks y Custom Hooks

--- src/components/pages/BadgeDetails.js
+++ src/components/pages/BadgeDetails.js
@@ -1,19 +1,30 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import { Link } from "react-router-dom";
 import "./styles/BadgeDetails.css";
-import ConfLogo from "../../images/platziconf-logo.svg";
+import confLogo from "../../images/platziconf-logo.svg";
 import Badge from "../Badge";
+import DeleteBadgeModal from "../DeleteBadgeModal";
+
+function useIncreaseCount(max) {
+  const [count, setCount] = React.useState(0);
+  if (count > max) {
+    setCount(0);
+  }
+
+  return [count, setCount];
+}
 
 function BadgeDetails(props) {
+  const [count, setCount] = useIncreaseCount(4);
   const badge = props.badge;
+
   return (
     <div>
       <div className="BadgeDetails__hero">
         <div className="container">
           <div className="row">
             <div className="col-6">
-              <img src={ConfLogo} alt="Logo de la Conferencia" />
+              <img src={confLogo} alt="Logo de la Conferencia" />
             </div>
             <div className="col-6 BadgeDetails__hero-attendant-name">
               <h1>
@@ -39,6 +50,14 @@ function BadgeDetails(props) {
             <h2>Actions</h2>
             <div>
               <div>
+                <button
+                  onClick={() => {
+                    setCount(count + 1);
+                  }}
+                  className="btn btn-primary mr-4"
+                >
+                  Increase Count {count}
+                </button>
                 <Link
                   className="btn btn-primary mb-4"
                   to={`/badges/${badge.id}/edit`}
@@ -48,11 +67,14 @@ function BadgeDetails(props) {
               </div>
 
               <div>
-                <button className="btn btn-danger">Delete</button>
-                {ReactDOM.createPortal(
-                  <h1>Hola, no estoy aquí realmente.</h1>,
-                  document.getElementById("modal")
-                )}
+                <button onClick={props.onOpenModal} className="btn btn-danger">
+                  Delete
+                </button>
+                <DeleteBadgeModal
+                  isOpen={props.modalIsOpen}
+                  onClose={props.onCloseModal}
+                  onDeleteBadge={props.onDeleteBadge}
+                />
               </div>
             </div>
           </div>
